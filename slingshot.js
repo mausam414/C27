e20978f0367d7af182d076fd8616ff30655bc0dd
mausam414 <78554556+mausam414@users.noreply.github.com>
2021-03-22 15:52:59 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,17 @@
+class slingshot {
+ constructor(bodyA,bodyB){
+  var options = {
+      bodyA:bodyA,
+      bodyB:bodyB,
+      stiffness:0.04,
+      length:10
+  }
+  this.sling=Constraint.create(options)
+  World.add(world,this.sling)
+    }
+display(){
+strokeWeight(4);
+line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.sling.bodyB.position.x,this.sling.bodyB.position.y)
+}
+
+}
